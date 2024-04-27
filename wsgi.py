e9bc7d8d5480c12e6f7697b2a30ad6e8db9dc8e1
peixoto98 <rp91058@saqueepague.com.br>
2024-04-27 19:34:45 -0300

    feat: add bandit for code scan

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from application import create_app
 import os
+import socket
 
 
 if os.getenv("FLASK_ENV") == "development":
@@ -7,5 +8,7 @@ if os.getenv("FLASK_ENV") == "development":
 else:
     app = create_app("config.ProdConfig")
 
+ip = socket.gethostbyname(socket.gethostname())
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", 5000))
+    app.run(debug=True, host=ip, port=os.getenv("PORT", 5000))
